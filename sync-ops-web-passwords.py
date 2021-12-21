faff61236665dc83ee26a699e70d452afc4fc5fd
William Jackson <william@subtlecoolness.com>
2021-12-21 12:26:18 -0600

    Simplify sync-ops-web-passwords

--- sync-ops-web-passwords.py
+++ sync-ops-web-passwords.py
@@ -17,7 +17,7 @@ def get_db_connection():
 def get_ops_web_data(cnx) -> list[dict]:
     with cnx:
         with cnx.cursor() as cur:
-            sql = 'select cloud, machine_id, password from windows_credentials'
+            sql = '''select cloud, machine_id, password from windows_credentials where password <> '_exported_' '''
             cur.execute(sql)
             data = cur.fetchall()
     return data
@@ -30,42 +30,40 @@ def update_ops_web_row(cnx, machine_id):
             cur.execute(sql, {'machine_id': machine_id})
 
 
-def get_secrets_list(ssc):
+def get_secrets_list(ssc) -> dict:
     params = {
         'filter.folderId': (os.getenv('SECRET_SERVER_FOLDER_ID')),
         'take': 100,
     }
-    return list(ssc.get_secrets(params))
+    return {s.get('name'): s.get('id') for s in ssc.get_secrets(params)}
 
 
 def main_job():
     cnx = get_db_connection()
-    ssc = secret_server.SecretServerClient()
     ops_web_rows = get_ops_web_data(cnx)
+    if len(ops_web_rows) < 1:
+        log.info(f'No new passwords in Ops Web')
+        return
+    elif len(ops_web_rows) > 1:
+        log.info(f'{len(ops_web_rows)} new passwords in Ops Web')
+    else:
+        log.info('1 new password in Ops Web')
+
+    ssc = secret_server.SecretServerClient()
     all_secrets = get_secrets_list(ssc)
-    thycotic_secret_names = [row.get('name') for row in all_secrets]
     for ops_web_row in ops_web_rows:
         cloud = ops_web_row.get('cloud')
         machine_id = ops_web_row.get('machine_id')
         candidate_secret_name = f'{cloud}.{machine_id}'
-        if candidate_secret_name in thycotic_secret_names:
-            if ops_web_row.get('password') == '_exported_':
-                log.info(f'{candidate_secret_name} is already saved in Secret Server')
-            else:
-                log.info(f'{candidate_secret_name} has an updated password')
-                secret_id = None
-                for secret in all_secrets:
-                    if secret.get('name') == candidate_secret_name:
-                        secret_id = secret.get('id')
-                        break
-                ssc.delete_secret(secret_id)
-                ssc.post_secrets(int(os.getenv('SECRET_SERVER_FOLDER_ID')), candidate_secret_name, 'Administrator',
-                                 ops_web_row.get('password'))
+        if candidate_secret_name in all_secrets:
+            log.info(f'{candidate_secret_name} has an updated password')
+            secret_id = all_secrets.get(candidate_secret_name)
+            ssc.delete_secret(secret_id)
         else:
             log.info(f'{candidate_secret_name} needs to be added to Secret Server')
-            ssc.post_secrets(int(os.getenv('SECRET_SERVER_FOLDER_ID')), candidate_secret_name, 'Administrator',
-                             ops_web_row.get('password'))
-            update_ops_web_row(cnx, ops_web_row.get('machine_id'))
+        folder_id = int(os.getenv('SECRET_SERVER_FOLDER_ID'))
+        ssc.post_secrets(folder_id, candidate_secret_name, 'Administrator', ops_web_row.get('password'))
+        update_ops_web_row(cnx, ops_web_row.get('machine_id'))
 
 
 def main():
